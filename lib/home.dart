57a432e3e50cc1cc8a31eddc8a7a5c789c83a320
Jens Becker <info@jensbecker.dev>
2021-07-26 22:11:31 +0200

    add fleasy's analysis options

--- lib/home.dart
+++ lib/home.dart
@@ -5,7 +5,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text('Home'),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'home.dart';
 
-void main() => runApp(MonopolyBankingApp());
+void main() => runApp(const MonopolyBankingApp());
 
 class MonopolyBankingApp extends StatelessWidget {
+  const MonopolyBankingApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Monopoly Banking',
       home: HomeScreen(),
     );
