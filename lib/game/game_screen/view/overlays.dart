92ffad64d21e45aede0f21d73d1191fa73ce9be7
Jens Becker <info@jensbecker.dev>
2021-09-01 19:34:39 +0200

    Change confetti behavior on results screen

--- lib/game/game_screen/view/overlays.dart
+++ lib/game/game_screen/view/overlays.dart
@@ -133,98 +133,103 @@ class _ResultsOverlayState extends State<ResultsOverlay> {
   Widget build(BuildContext context) {
     assert(widget.game.winner != null);
     final user = context.read<AppCubit>().state.user;
-    final winnerNameOrYou = widget.game.winner!.userId == user.id
-        ? 'You'
-        : widget.game.winner!.name;
+    final thisPlayerIsWinner = widget.game.winner!.userId == user.id;
+    final winnerNameOrYou =
+        thisPlayerIsWinner ? 'You' : widget.game.winner!.name;
     final gameDurationInMinutes = widget.game.duration.inMinutes;
 
-    //todo: confetti only for the one who won?!
-    //todo: confetti should come from the top middle of the screen, no matter how wide it is!
-    return ConfettiWidget(
-      confettiController: _confettiController,
-      blastDirection: pi / 2,
-      blastDirectionality: BlastDirectionality.explosive,
-      particleDrag: 0.05,
-      emissionFrequency: 0.05,
-      numberOfParticles: 25,
-      gravity: 0.05,
-      child: Container(
-        color: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.8),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const FaIcon(
-                Icons.emoji_events_rounded,
-                size: 75,
-              ),
-              const SizedBox(height: 15),
-              Text(
-                '$winnerNameOrYou won the game!',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              const SizedBox(height: 5),
-              Column(
-                children: widget.game.bankruptPlayersSortedByPlace
-                    .asList()
-                    .map((player) {
-                  final nameOrYou =
-                      player.userId == user.id ? 'You' : player.name;
-                  final bankruptTimeInMinutes =
-                      player.bankruptTime(widget.game).inMinutes;
-
-                  late final IconData icon;
-                  switch (player.place(widget.game)) {
-                    case 2:
-                      icon = Icons.looks_two_rounded;
-                      break;
-                    case 3:
-                      icon = Icons.looks_3_rounded;
-                      break;
-                    case 4:
-                      icon = Icons.looks_4_rounded;
-                      break;
-                    case 5:
-                      icon = Icons.looks_5_rounded;
-                      break;
-                    case 6:
-                      icon = Icons.looks_6_rounded;
-                      break;
-                    default:
-                      icon = Icons.error;
-                      break;
-                  }
-
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(icon),
-                      const SizedBox(width: 5),
-                      Text(
-                        '$nameOrYou (Bankrupt after $bankruptTimeInMinutes min)',
-                        style: const TextStyle(fontSize: 17),
-                      ),
-                    ],
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 25),
-              IconText(
-                text: Text(
-                  'Duration of the game: $gameDurationInMinutes min',
-                  style: const TextStyle(fontSize: 17),
+    return Stack(
+      children: [
+        Container(
+          color: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const FaIcon(
+                  Icons.emoji_events_rounded,
+                  size: 75,
                 ),
-                gap: 7,
-                icon: const FaIcon(
-                  FontAwesomeIcons.solidClock,
-                  size: 20,
+                const SizedBox(height: 15),
+                Text(
+                  '$winnerNameOrYou won the game!',
+                  style: Theme.of(context).textTheme.headline5,
                 ),
-                iconAfterText: false,
-              ),
-            ],
+                const SizedBox(height: 5),
+                Column(
+                  children: widget.game.bankruptPlayersSortedByPlace
+                      .asList()
+                      .map((player) {
+                    final nameOrYou =
+                        player.userId == user.id ? 'You' : player.name;
+                    final bankruptTimeInMinutes =
+                        player.bankruptTime(widget.game).inMinutes;
+
+                    late final IconData icon;
+                    switch (player.place(widget.game)) {
+                      case 2:
+                        icon = Icons.looks_two_rounded;
+                        break;
+                      case 3:
+                        icon = Icons.looks_3_rounded;
+                        break;
+                      case 4:
+                        icon = Icons.looks_4_rounded;
+                        break;
+                      case 5:
+                        icon = Icons.looks_5_rounded;
+                        break;
+                      case 6:
+                        icon = Icons.looks_6_rounded;
+                        break;
+                      default:
+                        icon = Icons.error;
+                        break;
+                    }
+
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(icon),
+                        const SizedBox(width: 5),
+                        Text(
+                          '$nameOrYou (Bankrupt after $bankruptTimeInMinutes min)',
+                          style: const TextStyle(fontSize: 17),
+                        ),
+                      ],
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(height: 25),
+                IconText(
+                  text: Text(
+                    'Duration of the game: $gameDurationInMinutes min',
+                    style: const TextStyle(fontSize: 17),
+                  ),
+                  gap: 7,
+                  icon: const FaIcon(
+                    FontAwesomeIcons.solidClock,
+                    size: 20,
+                  ),
+                  iconAfterText: false,
+                ),
+              ],
+            ),
           ),
         ),
-      ),
+        if (thisPlayerIsWinner)
+          Align(
+            alignment: Alignment.topCenter,
+            child: ConfettiWidget(
+              confettiController: _confettiController,
+              blastDirection: pi / 2,
+              blastDirectionality: BlastDirectionality.explosive,
+              emissionFrequency: 0.05,
+              numberOfParticles: 25,
+              //   gravity: 0.05,
+            ),
+          ),
+      ],
     );
   }
 }
