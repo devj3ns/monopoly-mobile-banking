49084173d309d06d7aec90745e697e8a640ca49b
Jens Becker <info@jhb.software>
2025-01-02 00:00:02 +0100

    feat(web): add vercel integration

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  adapter: vercel(),
 });
